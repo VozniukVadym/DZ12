d92981187d555f045990b37c070e808b4f81d08e
Vozniuk Vadym <Wozniukelektric@gmail.com>
2023-01-08 16:17:36 +0200

    midle

--- DZ12_1.py
+++ DZ12_1.py
@@ -1,13 +1,13 @@
-import threading
+import concurrent.futures
 import time
 def factorial(x):
     f=1
     for i in range(1, x+1):
         f*=i
     return f
-
-a = threading.Thread(target=factorial, args={5})
 start_time=time.time()
-a.start()
-a.join()
+print(factorial(5))
+print(factorial(10))
+print(factorial(20))
+print(factorial(50))
 print(time.time()-start_time)
